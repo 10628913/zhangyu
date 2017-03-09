548463dab3858174bd0806ff0e58d4ed5c2ea8f2
李文林 <346608148@qq.com>
2017-03-09 21:00:08 +0800

    update

--- Application/App/Controller/AppController.class.php
+++ Application/App/Controller/AppController.class.php
@@ -36,7 +36,7 @@ class AppController extends RestController {
         }
     }
     //通用ajax返回
-    function httpResponse($code,$msg,$data = array(),$type = "json"){
+    function httpResponse($code,$msg,$data,$type = "json"){
         $ret["code"] = $code;
         $ret["msg"] = $msg;
         if($data){
